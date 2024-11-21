fbae391c9c66561163a9aa0cc9b75714a43fb382
KienVu102 <vudinhkien1002@gmail.com>
2024-11-21 10:04:17 +0700

    Kiên Vũ upload Session02_Ex04

---
+++ Session02_Ex04.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int chieudai = 6;
+	int chieurong = 9;
+	int chuvi = (chieudai+chieurong)*2;
+	int dientich = chieudai*chieurong;
+	printf("Chieu dai cua hinh chu nhat la %d \n", chieudai);
+	printf("Chieu rong cua hinh chu nhat la %d \n", chieurong);
+	printf("Chu vi cua hinh chu nhat la %d \n", chuvi);
+	printf("Dien tich cua hinh chu nhat la %d \n", dientich);
+	return 0;
+}
