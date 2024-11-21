076daf4a52e6bc3a914f6cb7b21b07e1bd1a61d2
KienVu102 <vudinhkien1002@gmail.com>
2024-11-21 10:04:02 +0700

    Kiên Vũ upload Session02_Ex03

---
+++ Session02_Ex03.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a=10;
+	int b=20;
+	int E, F, G;
+	E=a-b;
+	F=a*b;
+	G=a/b;
+	printf("Hieu cua %d va %d la: %d \n", a, b, E);
+	printf("Tich cua %d va %d la: %d \n", a, b, F);
+	printf("Thuong cua %d va %d la: %d \n", a, b, G);
+	
+	return 0;
+}
+
